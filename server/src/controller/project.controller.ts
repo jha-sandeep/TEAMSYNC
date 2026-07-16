47295cf23e7cd58496c1ced2826169375a44a63b
jha-sandeep <sandeep100022@gmail.com>
2026-07-16 20:11:40 +0530

    CRUD api

--- server/src/controller/project.controller.ts
+++ server/src/controller/project.controller.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
-import { createProject } from "../services/project.service.js";
+import { createProject, updateProject, deleteProject, getProjects,getProjectById } from "../services/project.service.js";
 
 export async function createProjectHandler(
   req: Request,
@@ -13,11 +13,80 @@ export async function createProjectHandler(
   const project = await createProject({
     name,
     description,
-    ownerId,
-  });
+
+  }, ownerId,);
 
   res.status(201).json({
     success: true,
     data: project,
   });
+}
+
+
+type UpdateProjectParams = {
+  id: string;
+};
+
+export async function updateProjectHandler(
+  req: Request<UpdateProjectParams>,
+  res: Response
+) {
+  try {
+    const project = await updateProject(req.params.id, req.body);
+    return res.status(200).json({ success: true, data: project });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(404).json({ success: false, message: error.message });
+    }
+
+    return res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+}
+
+export async function deleteProjectHandler(
+  req: Request<UpdateProjectParams>,
+  res: Response
+) {
+  try {
+    const project = await deleteProject(req.params.id);
+    return res.status(200).json({ success: true, message: "Project deleted successfully" });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(404).json({ success: false, message: error.message });
+    }
+
+    return res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+}
+
+export async function getProjectsHandler(
+  req: Request,
+  res: Response
+) {
+  try {
+    const project = await getProjects();
+    return res.status(200).json({ success: true, data: project });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(404).json({ success: false, message: error.message });
+    }
+
+    return res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
+}
+
+export async function getProjectByIdHandler(
+  req: Request<{ id: string }>,
+  res: Response
+) {
+  try {
+    const project = await getProjectById(req.params.id);
+    return res.status(200).json({ success: true, data: project });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(404).json({ success: false, message: error.message });
+    }
+
+    return res.status(500).json({ success: false, message: "Internal Server Error" });
+  }
 }
